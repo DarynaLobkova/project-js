bee38f8efc5db890441d1abf141f578dcc2290ad
Daryna Lobkova <darinka021193@gmail.com>
2022-10-17 17:17:17 +0200

    update hw 6

--- js/hw6-js/test1.js
+++ js/hw6-js/test1.js
@@ -157,11 +157,42 @@ const users =  [
                                   // Задание 8
 // Массив имен всех пользователей у которых есть друг с указанным именем.
 
-const getUsersWithFriend = (array, name) =>
-  array.find(({ friends }) => friends === name)
- 
- 
+// const allFriends = users.reduce((allFriends, user) => {
+// allFriends.push(...user.friends);
+// return allFriends;
+// }, []);
+// console.log(allFriends);
+
+// const getUsersWithFriend = (users, friendName) =>
 
 
-console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
+// console.log(getUsersWithFriend(users, 'Briana Decker')); 
+// console.log(getUsersWithFriend(users, 'Goldie Gentry')); 
+
+
+
+                                   // Задание 9
+// Массив имен(поле name) людей, отсортированных
+//  в зависимости от количества их друзей(поле friends)
+
+// const getNamesSortedByFriendsCount = 
+ 
+
+// console.log(getNamesSortedByFriendsCount(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+
+                                   // Задание 10
+// Получить массив всех умений всех пользователей(поле skills),
+//   при этом не должно быть повторяющихся умений и они должны быть
+//    отсортированы в алфавитном порядке.
+
+const getSortedUniqueSkills = users => users.reduce(( skills, user) => {
+  skills.push(...user.skills)
+  skills.sort();
+
+  return skills;
+}, []);
+  
+
+console.log(getSortedUniqueSkills(users));
